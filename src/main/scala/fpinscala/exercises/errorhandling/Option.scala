6b041920960dac6031dea169480c5749a7ab7679
lai <doublewlai@gmail.com>
2022-05-02 21:38:39 +0300

    Option

--- src/main/scala/fpinscala/exercises/errorhandling/Option.scala
+++ src/main/scala/fpinscala/exercises/errorhandling/Option.scala
@@ -7,15 +7,30 @@ enum Option[+A]:
   case Some(get: A)
   case None
 
-  def map[B](f: A => B): Option[B] = ???
+  def map[B](f: A => B): Option[B] =
+    this match {
+      case None => None
+      case Some(a) => Some(f(a))
+    }
 
-  def getOrElse[B>:A](default: => B): B = ???
+  def getOrElse[B>:A](default: => B): B =
+    this match {
+      case None => default
+      case Some(a) => a
+    }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = ???
+  def flatMap[B](f: A => Option[B]): Option[B] =
+    map(f).getOrElse(None)
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = ???
+  def orElse[B>:A](ob: => Option[B]): Option[B] =
+    map(Some(_)).getOrElse(ob)
 
-  def filter(f: A => Boolean): Option[A] = ???
+
+  def filter(f: A => Boolean): Option[A] =
+    this match {
+      case Some(a) if (f(a)) => this
+      case _ => None
+    }
 
 object Option:
 
